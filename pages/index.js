b377168aef22557e17191f927f632c327241ce45
Pedro Dutra <pedrodutra86@hotmail.com>
2025-04-16 15:54:58 -0300

    feat: add lint:check and lint:fix scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Hey, if you're reading this, you are incredible! 😎</h1>
+  return <h1>Hey, if you're reading this, you are incredible! 😎</h1>;
 }
 
 export default Home;
