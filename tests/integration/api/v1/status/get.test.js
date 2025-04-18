728d7b1fe5268ed933dea3d051f7ab41dba7cf68
Pedro Dutra <pedrodutra86@hotmail.com>
2025-04-18 19:44:18 -0300

    feat: finish api/v1/status endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,4 +2,13 @@ test("GET to /api/v1/status must return 200", async () => {
   const request = await fetch("http://localhost:3000/api/v1/status");
   const response = request;
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
